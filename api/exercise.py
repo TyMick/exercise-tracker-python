d45946447847da93309fa6ba830da4672020545b
Ty Mick <ty@tymick.me>
2020-09-28 17:37:58 -0400

    Add add_exercise

--- api/exercise.py
+++ api/exercise.py
@@ -1,20 +1,11 @@
 from flask import request, jsonify, redirect
 from database import get_db
+from datetime import date as py_date
+import sqlite3
 import nanoid
 
 
-def dict_factory(cursor, row):
-    """
-    Turns rows into dictionaries for easier JSON conversion. Plugs into
-    Connection.row_factory.
-    """
-    d = {}
-    for idx, col in enumerate(cursor.description):
-        d[col[0]] = row[idx]
-    return d
-
-
-def create_new_user(): # test _id: J9LyZZ9VPD
+def create_new_user():
     username = request.form["username"]
     _id = nanoid.generate(
         alphabet="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
@@ -27,24 +18,75 @@ def create_new_user(): # test _id: J9LyZZ9VPD
             "INSERT INTO User(_id, username) VALUES(?, ?)", (_id, username)
         )
         db.commit()
-        return jsonify(username=username, _id=_id)
+        return {"username": username, "_id": _id}
+    except sqlite3.IntegrityError as e:
+        if e.args[0] == "UNIQUE constraint failed: User.username":
+            return {"error": "Username already exists"}
+        elif e.args[0] == "CHECK constraint failed: User":
+            return {"error": "Username cannot be longer than 32 characters"}
     except:
-        return jsonify(error="database error")
+        return {"error": "Database error"}
 
 
 def get_all_users():
     try:
         db = get_db()
-        db.row_factory = dict_factory
         c = db.cursor()
         c.execute("SELECT username, _id FROM User")
-        return jsonify(c.fetchall())
+        users = c.fetchall()
+        return jsonify(users)
     except:
-        return jsonify(error="database error")
+        return {"error": "Database error"}
 
 
 def add_exercise():
-    pass
+    # Validate date input
+    if request.form["date"]:
+        try:
+            date = py_date.fromisoformat(request.form["date"])
+        except ValueError:
+            return {"error": "Invalid date input"}
+    else:
+        date = py_date.today()
+
+    try:
+        db = get_db()
+        c = db.cursor()
+
+        # Check for userId
+        user_id = request.form["userId"]
+        c.execute("SELECT username, _id FROM User WHERE _id = ?", (user_id,))
+        user = c.fetchone()
+        if user is None:
+            return {"error": "No such userId"}
+
+        username = user["username"]
+        description = request.form["description"]
+        duration = int(request.form["duration"])
+        c.execute(
+            """
+            INSERT INTO Exercise(username, userId, description, duration, date)
+            VALUES(?, ?, ?, ?, ?)
+            """,
+            (username, user_id, description, duration, date.isoformat()),
+        )
+        db.commit()
+
+        return {
+            "username": username,
+            "userId": user_id,
+            "description": description,
+            "duration": duration,
+            "date": date.isoformat(),
+        }
+
+    except sqlite3.IntegrityError as e:
+        if "description_length" in e.args[0]:
+            return {"error": "Description cannot be longer than 32 characters"}
+        elif "duration_minimum" in e.args[0]:
+            return {"error": "Duration must be positive"}
+    except:
+        return {"error": "Database error"}
 
 
 def get_exercise_log():
--- database.py
+++ database.py
@@ -4,10 +4,22 @@ import sqlite3
 DATABASE = "exercise_tracker.db"
 
 
+def dict_factory(cursor, row):
+    """
+    Turns rows into dictionaries for easier JSON conversion. Plugs into
+    Connection.row_factory.
+    """
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+
 def get_db():
     db = getattr(g, "_database", None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
+        db.row_factory = dict_factory
     return db
 
 
@@ -27,9 +39,11 @@ def init_db():
         CREATE TABLE IF NOT EXISTS Exercise(
             username TEXT,
             userId TEXT NOT NULL,
-            description TEXT NOT NULL,
-            duration INTEGER NOT NULL CHECK (duration > 0),
-            date REAL NOT NULL
+            description TEXT NOT NULL
+                CONSTRAINT description_length CHECK (length(description) <= 32),
+            duration INTEGER NOT NULL
+                CONSTRAINT duration_minimum CHECK (duration > 0),
+            date TEXT NOT NULL
         )
         """
     )
