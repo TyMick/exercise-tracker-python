32993950580da5fe4b39c65d743759563a4df3fc
Ty Mick <ty@tymick.me>
2020-09-28 14:31:46 -0400

    Add get_all_users

--- api/exercise.py
+++ api/exercise.py
@@ -3,6 +3,17 @@ from database import get_db
 import nanoid
 
 
+def dict_factory(cursor, row):
+    """
+    Turns rows into dictionaries for easier JSON conversion. Plugs into
+    Connection.row_factory.
+    """
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+
 def create_new_user(): # test _id: J9LyZZ9VPD
     username = request.form["username"]
     _id = nanoid.generate(
@@ -18,11 +29,18 @@ def create_new_user(): # test _id: J9LyZZ9VPD
         db.commit()
         return jsonify(username=username, _id=_id)
     except:
-        return jsonify(error="database error", code=500)
+        return jsonify(error="database error")
 
 
 def get_all_users():
-    pass
+    try:
+        db = get_db()
+        db.row_factory = dict_factory
+        c = db.cursor()
+        c.execute("SELECT username, _id FROM User")
+        return jsonify(c.fetchall())
+    except:
+        return jsonify(error="database error")
 
 
 def add_exercise():
